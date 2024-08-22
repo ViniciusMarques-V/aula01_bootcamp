3688322593854af53dcd2e2744751af8383433cd
Vinicius <vinicius.marqques@gmail.com>
2024-08-21 21:27:19 -0300

    Adicionar primeiro exemplo de exercicio

--- exemplos_00.py
+++ exemplos_00.py
@@ -1,5 +1,15 @@
-#Contando a quantidade de caraceteres#
+#Calculando Bonus#
 
+CONSTANTE_BONUS = 1000
 
-print(int(input("digite um numero: ")) + 
-int(input("digite outro numero: ")) )
+nome = input("Digite o nome: ")
+salario = input("Digite o pagode do cidadão: ")
+perc_bonus = input("Digite o Bonus: ")
+
+salario = float(salario)
+perc_bonus = float(perc_bonus)
+
+vlr_bonus =  salario * perc_bonus + CONSTANTE_BONUS
+
+print(f"O usuario {nome} possui o bonus de {vlr_bonus}")
+#print(vlr_bonus)
